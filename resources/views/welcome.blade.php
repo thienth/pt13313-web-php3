0f367594153a29f01e2221b685a4abe79914d035
tuanskyxox <tuananhnguyen199dz@gmail.com>
2019-07-08 14:09:18 +0700

    update tuan branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    FPT Polytechnic
+                    FPT Polytechnic _ Tuan dep trai
                 </div>
 
                 
